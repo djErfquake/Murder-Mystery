de0928c48b8b45cc48ca4acee088efe08a9bf46e
clevally <clevally@roto.com>
2019-02-01 16:41:04 -0500

    initial commit

---
+++ app/mm/mm.js
@@ -0,0 +1,65 @@
+$(function () {
+
+  $('.code-button').on('click', () => {
+    let code = $('.code-entry').val().toUpperCase();
+    //console.log("code", code);
+
+    let requestUrl = location.protocol + '//' + location.host + '/mm/api?code=' + code;
+    //console.log("sending request to ", requestUrl);
+
+    $.getJSON(requestUrl, (data) => {
+
+      console.log("response", data);
+
+
+      if ('error' in data) {
+        console.log("WRONG");
+        Swal.fire({
+          title: 'Error!',
+          text: "That isn't a valid code!",
+          type: 'error',
+          confirmButtonText: 'Oops'
+        });
+      } else {
+        let character = data.character;
+        $('.code-form').remove();
+        loadCharacter(character);
+
+
+        $('.how-it-works-section').removeClass('hidden');
+
+
+        $('#character-icon').on('click', () => {
+          $('html, body').animate({scrollTop: $('.name-section').offset().top}, 1000);
+        });
+
+        $('#question-icon').on('click', () => {
+          $('html, body').animate({scrollTop: $('.how-it-works-section').offset().top}, 1000);
+        });
+
+      }
+    });
+
+  });
+
+
+
+
+});
+
+
+let loadCharacter = function(character) {
+  // https://tutorialzine.com/2015/01/learn-handlebars-in-10-minutes
+
+  // retrieve the template data from the html
+  let template = $('#handlebars-character').html();
+
+  // compile the template data into a function
+  let templateScript = Handlebars.compile(template);
+
+  // create html code from context
+  let html = templateScript(character);
+
+  // insert the html
+  $(document.body).append(html);
+}
---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'app')));
+
+// serve these pages
+app.get('/mm', (req, res) => {
+  //res.send("Hello World!"));
+  res.status(200).sendFile(path.resolve(__dirname, '', 'app/mm', 'mm.html'));
+});
+
+app.get('/mm/api', (req, res) => {
+  if (req.query.code in people) {
+    res.json(people[req.query.code]);
+  } else {
+    res.json({error: "no person with that code."});
+  }
+});
+
+// start listening for requests
+app.listen(port, () => { console.log(`Listening on port ${port}`); });
+
+
+
+
+
+
+
+
+
+let abilities = {
+  pickpocket: {name: "Pick-Pocket", desc: "steal money blah blah blah"},
+  letsGossip: {name: "Let's Gossip", desc: "steal money blah blah blah"},
+  aMomentOfPassion: {name: "A Moment of Passion", desc: "steal money blah blah blah"}
+};
+
+
+// setup character data here
+let blaiseSadler = {
+  name: "Blaise Sadler",
+  gender: "F",
+  desc: "an aging saloon madame who is known to be selfish and unscrupulous",
+  scenarioInfo: "",
+  relations: [
+    "You are Kalamata Kate's Boss"
+  ],
+  items: [
+    "Half of a map to Zeke Calhoun's mine, including Dead Man's Bluff",
+    "3 marked playing cards",
+    "$21"
+  ],
+  abilities: [
+    abilities.pickpocket,
+    abilities.letsGossip,
+    abilities.aMomentOfPassion
+  ],
+  goals: [
+    "Get $2000 so you can retire OR Get a share in Zeke Calhoun's mine",
+    "Decide what to do with the marked cards",
+    "Decide what to do with your half of the map"
+  ],
+  secrets: [
+    "You saw <b>Doc Fairaday</b> cheat against Zeke Calhoun, and you have the cards to prove it",
+    "Zeke Calhoun gave you part of the map to his silver mine before he died"
+  ],
+  clue: "There is something sinister about Elijah Entwhistle"
+};
+
+
+
+
+let people = {
+  "DUCK": {person: "Carolyn", character: blaiseSadler}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TO RUN
+// node --inspect index.js
